3a6485770c70f97cc66fd5a5959c058e03e3cfc5
VEDANT TAPASE <vedanttapase70@gmail.com>
2026-07-05 16:57:29 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   typescript: {
     // Dangerously allow production builds to successfully complete even if
     // your project has TypeScript errors.
